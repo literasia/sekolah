b781dae62efa4843a5919792ec1049f82f3e3697
syafri maradu manurung <syafrimaradu@gmail.com>
2021-01-05 15:09:33 +0700

    tambah sekolah sudah selesai

--- resources/views/superadmin/modals/_tambah-sekolah.blade.php
+++ resources/views/superadmin/modals/_tambah-sekolah.blade.php
@@ -20,9 +20,78 @@
                         </div>
                       </div>
                       <div class="row">
+                        <div class="col-md-6">
+                          <div class="form-group bmd-form-group">
+                            <label class="bmd-label-floating">ID Sekolah</label>
+                            <input type="text" name="nama_sekolah" id="nama_sekolah" class="form-control form-control-sm" placeholder="ID Sekolah">
+                          </div>
+                        </div>
+                        <div class="col-md-6">
+                          <div class="form-group bmd-form-group">
+                            <label class="bmd-label-floating">Kepala Sekolah</label>
+                            <input type="text" name="nama_sekolah" id="nama_sekolah" class="form-control form-control-sm" placeholder="Kepala Sekolah">
+                          </div>
+                        </div>
+                      </div> 
+                      <div class="row">
+                        <div class="col-md-6">
+                          <div class="form-group bmd-form-group">
+                            <select class="form-control" aria-label="Default select example">
+                              <option selected>Kota / Kabupaten</option>
+                              <option value="1">Medan</option>
+                              <option value="2">Pematangsiantar</option>
+                              <option value="3">Asahan</option>
+                              <option value="4">Simalungun</option>
+                              <option value="5">Toba</option>
+                              <option value="6">Samosir</option>
+                            </select>
+                          </div>
+                         </div>
+                        <div class="col-md-6">
+                          <div class="form-group bmd-form-group">
+                           <select class="form-control" aria-label="Default select example">
+                        <option selected>Provinsi</option>
+                        <option value="1">Sumatra Utara</option>
+                        <option value="2">Sumatra barat</option>
+                        <option value="3">Riau</option>
+                        <option value="4">Kepulauan Riau</option>
+                        <option value="5">Jambi</option>
+                        <option value="6">Bengkulu</option>
+                        <option value="7">Sumatra Selatan</option>
+                        <option value="8">Kepulauan Bangka Belitung</option>
+                        <option value="9">Lampung</option>
+                        <option value="10">Banten</option>
+                        <option value="11">DKI Jakarta</option>
+                        <option value="12">Jawa Barat</option>
+                        <option value="13">Jawa Tengah</option>
+                        <option value="14">Jawa Timur</option>
+                        <option value="15">DI Yogyakarta</option>
+                        <option value="16">Bali</option>
+                        <option value="17">Nusa Tenggara Barat</option>
+                        <option value="18">Nusa Tenggara Timur</option>
+                        <option value="19">Kalimantan Barat</option>
+                        <option value="20">Kalimantan Selatan</option>
+                        <option value="21">Kalimantan Tengah</option>
+                        <option value="22">Kalimantan Timur</option>
+                        <option value="23">Kalimantan Utara</option>
+                        <option value="24">Gorontalo</option>
+                        <option value="25">Sulawesi Barat</option>
+                        <option value="26">Sulawesi Selatan</option>
+                        <option value="27">Sulawesi Tenggara</option>
+                        <option value="28">Sulawesi Tengah</option>
+                        <option value="29">Sulawesi Utara</option>
+                        <option value="30">Maluku</option>
+                        <option value="31">Maluku Utara</option>
+                        <option value="32">Papua</option>
+                        <option value="33">Papua Barat</option>
+                        </select>
+                          </div>
+                        </div>
+                      </div>  
+                      <div class="row">
                         <div class="col-md-12">
-                          <label>Alamat :</label>
                           <div class="form-group">
+                          <label>Alamat :</label>
                             <textarea class="form-control form-control-sm" name="alamat_sekolah" id="alamat_sekolah" rows="10" placeholder="Alamat Sekolah"></textarea>
                           </div>
                         </div>
@@ -30,9 +99,8 @@
                       <div class="row">
                         <div class="col-md-12">
                           <div class="text-right">
-                            <button type="submit" class="btn btn-sm btn-outline-success">Simpan</button>
-                            <button class="btn btn-sm btn-warning" type="reset" for="resetBtn">Reset</button>
                             <button type="button" class="btn btn-sm btn-danger" data-dismiss="modal">Batal</button>
+                            <button type="submit" class="btn btn-sm btn-outline-success">Simpan</button>                          
                           </div>
                         </div>
                       </div>
