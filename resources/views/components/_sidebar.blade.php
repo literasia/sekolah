7717e517c15b583aa62d441c51703fbc37dfcd26
yudabp <tama.yuda48@gmail.com>
2021-02-21 06:01:30 +0700

    sidebar

--- resources/views/components/_sidebar.blade.php
+++ resources/views/components/_sidebar.blade.php
@@ -1,14 +1,14 @@
 <nav class="pcoded-navbar">
     <div class="nav-list">
         <div class="pcoded-inner-navbar main-menu">
-            @if ($mySekolah ?? '')
-                @if ($mySekolah ?? ''->logo)
+            @if ($mySekolah)
+                @if ($mySekolah->logo)
                     <a href="/admin" class="d-flex" style="justify-content: center;">
-                        <img class="img-fluid" src="{{ Storage::url($mySekolah ?? ''->logo) }}" alt="logo sekolah" width="180" />
+                        <img class="img-fluid" src="{{ Storage::url($mySekolah->logo) }}" alt="logo sekolah" width="180" />
                     </a>
                 @endif
-                @if ($mySekolah ?? ''->name)
-                    <h3 style="color: white;" class="text-center mt-2">{{ $mySekolah ?? ''->name }}</h3>
+                @if ($mySekolah->name)
+                    <h3 style="color: white;" class="text-center mt-2">{{ $mySekolah->name }}</h3>
                 @endif
             @endif
             <div class="pcoded-navigation-label">Navigation</div>
