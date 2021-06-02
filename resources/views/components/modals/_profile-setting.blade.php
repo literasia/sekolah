f71ed63279a8e57a21075697e46de27a817a4d0e
nisasaaf <khairunissa@literasia.co.id>
2021-06-02 13:19:09 +0700

    y

--- resources/views/components/modals/_profile-setting.blade.php
+++ resources/views/components/modals/_profile-setting.blade.php
@@ -1,5 +1,4 @@
 <div class="modal fade modal-flex" id="modal-profile" tabindex="-1" role="dialog">
-    <!--  -->
     <div class="modal-dialog modal-lg" role="document"> 
         <div class="modal-content">
             <div class="modal-header">
