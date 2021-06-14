9abd2e5add7222a6a7544197396a101d3d3152f8
danang95 <danang.dev8@gmail.com>
2021-06-14 10:10:58 +0700

    fix-api-library

--- app/Http/Controllers/API/LibraryController.php
+++ app/Http/Controllers/API/LibraryController.php
@@ -225,15 +225,129 @@ class LibraryController extends Controller
         return response()->json(ApiResponse::success(['sub_kategori' => $sub_kategori]));
     }
 
-    public function getBuku(SubKategori $sub_kategori, Tingkat $tingkat, $sekolah_id, Request $request){
-        $library = Library::where(function ($query) use($sekolah_id){
+    public function getBuku(SubKategori $sub_kategori, Tingkat $tingkat, $sekolah_id, Request $req){
+        $data = $req->all();
+        $userId = $req->query('user_id');
+
+        $validator = Validator::make($data, [
+            'order_by' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(ApiResponse::validationError($validator->errors()));
+        }
+
+
+        $libraries = Library::where(function ($query) use($sekolah_id){
                                 $query->where('sekolah_id', $sekolah_id)
                                       ->orWhereNull('sekolah_id');
                           })
                           ->where('tingkat_id', $tingkat->id)
                           ->where('sub_kategori_id', $sub_kategori->id)
-                          ->get();
+                          ->with(['penulis', 'subKategori']);
 
-        return response()->json(ApiResponse::success(['library' => $library]));
+        // Search By Sekolah
+        $sekolahId = $req->query('sekolah_id');
+        $libraries->when($sekolahId, function($query) use ($sekolahId) {
+            return $query->where('sekolah_id', $sekolahId)
+                ->orWhereNull('sekolah_id');
+        });
+
+        // Search By Sub Kategori
+        // $subKategoriId = $req->query('sub_kategori_id');
+        // $libraries->when($subKategoriId, function($query) use ($subKategoriId) {
+        //     return $query->where('sub_kategori_id', $subKategoriId);
+        // });
+
+        // Search by Name Type
+        $q = $req->query('q');
+        $libraries->when($q, function($query) use ($q) {
+            return $query->whereRaw("name LIKE '%" . strtolower($q) . "%'");
+        });
+
+        // Search By 
+        $filterType = $req->query('filter_type');
+        $filterFunc = null;
+        if ($filterType == 'ebook') {
+            $filterFunc = function($query) {
+                return $query->whereNotNull('link_ebook')->where('link_ebook', '!=', '');
+            };
+        } else if ($filterType == 'audio') {
+            $filterFunc = function($query) {
+                return $query->whereNotNull('link_audio')->where('link_audio', '!=', '');
+            };
+        } else if ($filterType == 'video') {
+            $filterFunc = function($query) {
+                return $query->whereNotNull('link_video')->where('link_video', '!=', '');
+            };
+        }
+
+        if ($filterFunc != null) {
+            $libraries->when($filterType, $filterFunc);
+        }
+
+        $skipData = ((empty($data['page']) ? 1 : $data['page']) - 1) * 30;
+
+        $orderBy = $data['order_by'];
+        if ($orderBy == "borrowed" && !empty($userId)) {
+            $libraries = $libraries->whereHas('pinjams', function($q) use ($userId) {
+                $q->where('user_id', $userId);
+            });
+        } 
+
+        $totalLibraryEloquent = clone $libraries;
+        
+        switch ($orderBy) {
+            case "popular":
+                $libraries = $libraries->orderByDesc('viewed')
+                    ->orderBy('name')
+                    ->skip($skipData)
+                    ->take(30);
+                break;
+            case "newest":
+                $libraries = $libraries->orderByDesc('created_at')
+                    ->orderBy('name')
+                    ->skip($skipData)
+                    ->take(30);
+            default:
+                $libraries = $libraries->orderBy('name')->limit(30);
+        }
+
+        $totalLibraryByCategories = [
+            'audio' => 0,
+            'video' => 0,
+            'ebook' => 0
+        ];
+        
+        if (!empty($userId)) {
+            if ($orderBy == "like") {
+                $totalLibraryEloquent = $totalLibraryEloquent->join('user_library_likes AS like', 'like.library_id', 'libraries.id')
+                        ->where('like.user_id', $userId);
+                        
+                $libraries = $libraries->select(DB::raw("'true' AS is_liked, libraries.*"))
+                    ->join('user_library_likes AS like', 'like.library_id', 'libraries.id')
+                    ->where('like.user_id', $userId)
+                    ->get();
+            } else {
+                $libraries = $libraries->get();
+                foreach($libraries as $library) {
+                    $library['is_liked'] = UserLibraryLike::where([
+                        ['user_id', $userId],
+                        ['library_id', $library->id]
+                    ])->count() > 0;
+                }
+            }
+        } else {
+            $libraries = $libraries->get();
+        }
+        
+        $response = ApiResponse::success($libraries);
+        $response['total'] = [
+            'audio' => $this->getTotalLibraryByCategory($totalLibraryEloquent, 'link_audio'),
+            'video' => $this->getTotalLibraryByCategory($totalLibraryEloquent, 'link_video'),
+            'ebook' => $this->getTotalLibraryByCategory($totalLibraryEloquent, 'link_ebook')
+        ];        
+
+        return response()->json($response);
     }
 }
