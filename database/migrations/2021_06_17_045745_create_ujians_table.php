7fa08eaa08f2b2cb9d0fd081b71c1871457618dc
RinaldyKarokaro <rinaldikarokaro@gmail.com>
2021-06-22 10:29:02 +0700

    update cbt

--- database/migrations/2021_06_17_045745_create_ujians_table.php
+++ database/migrations/2021_06_17_045745_create_ujians_table.php
@@ -22,7 +22,7 @@ class CreateUjiansTable extends Migration
             $table->string('kuis');
             $table->string('status');
             $table->string('jenis_kuis');
-            $table->string('keterangan');
+            $table->text('keterangan');
             $table->date('tanggal_mulai');
             $table->date('tanggal_selesai');
             $table->foreign('soal_id')->references('id')->on('cbt_soals')->onDelete('cascade');
--- resources/views/admin/cbt/butir-soal-ujian.blade.php
+++ resources/views/admin/cbt/butir-soal-ujian.blade.php
@@ -229,6 +229,7 @@
         let alphabet = ['A', 'B', 'C', 'D', 'E', 'F'];
         
         $("#addButton").click(function () {
+            counter = 1;
             if(counter >= 6){
                 Swal.fire('Perhatian!', 'Hanya boleh 6 input form saja!', 'warning');
                 return false;
