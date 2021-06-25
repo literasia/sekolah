4ccff11674b73ee81a0f088f16790a58e8432495
nang95 <rookie95cookies@gmail.com>
2021-06-25 09:54:05 +0700

    forum-migration

---
+++ database/migrations/2021_06_25_023321_create_forums_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateForumsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('forums', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('topik_id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('judul');
+            $table->string('total_balasan');
+            $table->string('privasi');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('topik_id')->references('id')->on('topiks')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('forums');
+    }
+}
